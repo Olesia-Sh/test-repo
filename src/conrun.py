027f06c25346384e877f61be502d50a074eced8b
Olesia-Sh <147209141+Olesia-Sh@users.noreply.github.com>
2023-12-09 00:13:39 -0500

    Update conrun.py

--- src/conrun.py
+++ src/conrun.py
@@ -1,37 +1,14 @@
 #!/usr/bin/python3
 import mysql.connector
-import time
 
 class Mysql():
     def __init__(self, host, user, password):
+            self.conn = mysql.connector.connect(
+            host=host,
+            user=user,
+            password=password
 
-        max_retries = 30
-        retries = 0
-
-
-        while True:
-
-            try:
-
-               self.conn = mysql.connector.connect(
-                   host=host,
-                   user=user,
-                   password=password
-
-               )
-
-               print("MySql is ready")
-               break
-            except mysql.connector.Error as err:
-               print(f"Waiting for MySql....({err})")
-               time.sleep(1)
-               retries += 1
-
-               if retries >= max_retries:
-                    print("Unable to connect to MySql")
-                    exit(1)
-         
-
+        )
             self.cursor = self.conn.cursor()
     def connect_db(self):
         self.conn.connect()
